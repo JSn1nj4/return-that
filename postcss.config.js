bad0044d075461338b41211f5da13d4144b76fe1
Elliot Derhay <elliot@elliotderhay.com>
2023-06-10 13:12:15 -0400

    Tailwind + PostCSS setup

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	plugins: {
+		'postcss-import': {},
+		'postcss-mixins': {},
+		'postcss-simple-vars': {},
+		'tailwindcss/nesting': {},
+		tailwindcss: {},
+		autoprefixer: {},
+	},
+}

7944fe998ffd66f7c05127fca6c6b1f7b2102acf
Elliot Derhay <elliot@elliotderhay.com>
2023-12-28 17:40:47 -0500

    Remove some old class deps

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,21 +3,18 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UserResource extends Resource
 {
-    protected static ?string $model = User::class;
+    protected static string|null $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static string|null $navigationIcon = 'heroicon-o-collection';
 
     public static function form(Form $form): Form
     {

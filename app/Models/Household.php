bd00d542332b3f68513aed8254c52fb4a9c8f607
Elliot Derhay <elliot@elliotderhay.com>
2024-01-01 22:52:11 -0500

    Fix autocomplete for models

--- app/Models/Household.php
+++ app/Models/Household.php
@@ -6,6 +6,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * App\Models\Household
+ *
+ * @property int $id
+ * @property string $nickname
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $users
+ * @property-read int|null $users_count
+ * @method static \Database\Factories\HouseholdFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Household newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Household newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Household query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Household whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Household whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Household whereNickname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Household whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Household extends Model
 {
     use HasFactory;
